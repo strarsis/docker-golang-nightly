d2dd93bc9e7f903fdde58f77565e390d161383ae
build <build@infra-1.local.wdha>
2016-02-18 23:02:04 +0100

    Fix sample path for ssh key.

--- generate/config.example/github-repo-auth.js
+++ generate/config.example/github-repo-auth.js
@@ -3,5 +3,5 @@ var nodegit = require('nodegit'),
 
 module.exports = function(url, userName) {
   // return cred.sshKeyFromAgent(userName);
-  return cred.sshKeyNew(userName, '/home/build/github_id_rsa.pub', '/home/build/github_id_rsa', '');
+  return cred.sshKeyNew(userName, '/home/build/.ssh/github_id_rsa.pub', '/home/build/.ssh/github_id_rsa', '');
 }
