d7605d58a8622d62033fdd3bf93e6e765ace3d16
strarsis <strarsis@gmail.com>
2016-02-26 20:38:05 +0100

    Add comment to example github repo auth config.

--- generate/config.example/github-repo-auth.js
+++ generate/config.example/github-repo-auth.js
@@ -1,6 +1,7 @@
 var nodegit = require('nodegit'),
        cred = nodegit.Cred;
 
+// remote must use ssh:// protocol when returning a ssh key credential
 module.exports = function(url, userName) {
   // return cred.sshKeyFromAgent(userName);
   return cred.sshKeyNew(userName, '/home/build/.ssh/github_id_rsa.pub', '/home/build/.ssh/github_id_rsa', '');
